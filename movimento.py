cb632bc6bfcea729a9c705ba59cf0d25111edaf2
Gabriel Desiderá <gabriel.desidera@inspira.com.br>
2022-10-03 16:17:08 -0300

    Versão funcional

--- movimento.py
+++ movimento.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from multiprocessing.resource_sharer import stop
+from screeninfo import get_monitors
 import websocket
 import _thread
 import time
@@ -6,6 +8,8 @@ import rel
 import json
 import pyautogui
 import math
+
+pyautogui.FAILSAFE = False
 
 
 @dataclass(frozen=True)
@@ -28,13 +32,27 @@ class MarkerInfo:
 
 markers = [None, None]
 
+h_size_pixels, v_size_pixels = pyautogui.size()
+    
+monitors = get_monitors()
+
+h_size_cm, v_size_cm = (monitors[0].width_mm/10, monitors[0].height_mm/10)
+print(h_size_cm, v_size_cm)
+
+v_density = v_size_pixels / v_size_cm
+h_density = h_size_pixels / h_size_cm
+print(f"v_density: {v_density}, h_density: {h_density}")
+
+message_counter = 0
+
 
 def main():
 
     # Move mouse para o centro da tela:
-    h_size, v_size = pyautogui.size()
-    pyautogui.moveTo(h_size/2, v_size/2)
+    if len(monitors) != 1:
+        return
 
+    pyautogui.moveTo(h_size_pixels/2, v_size_pixels/2)
 
     # websocket.enableTrace(True)
     ws = websocket.WebSocketApp("ws://localhost:5678",
@@ -59,29 +77,46 @@ def get_coords():
 
     delta_right = current.rotation_right_z - previous.rotation_right_z
     delta_up = current.rotation_up_z - previous.rotation_up_z
-    delta_forward = current.rotation_forward_z - previous.rotation_forward_z
 
     z = (current.translation_z + previous.translation_z)/2
     
-    delta_x = math.tan(delta_right) * z
-    delta_y = math.tan(delta_up) * z
+    delta_x = math.tan(delta_right*math.pi) * z
+    delta_y = math.tan(delta_up*math.pi) * z
     
-    new_x = current_x + delta_x
-    new_y = current_y - delta_y
+    # print(f"delta_x: {delta_x}, delta_y: {delta_y}")
+
+    new_x = current_x - delta_x*h_density/10
+    new_y = current_y - delta_y*v_density/10
 
     return new_x, new_y
 
 def on_message(ws, message_json):
+
+    start_time = time.perf_counter_ns()
+
+    global message_counter
+
+    if message_counter < 10:
+        message_counter += 1
+        return
+    else:
+        message_counter = 0
+
     message_dict = json.loads(message_json)
     marker_info = MarkerInfo(**message_dict)
     
-    # if not marker_info.success: return
+    if not marker_info.success: 
+        return
     
     update_markers(marker_info)
 
     x, y = get_coords()
 
     pyautogui.moveTo(x, y)
+
+    stop_time = time.perf_counter_ns()
+
+    print(f"Elapsed time: {(stop_time - start_time)/1e6} ms")
     # print(f"coords: {x}, {y}")
     
     # print(marker_info)
