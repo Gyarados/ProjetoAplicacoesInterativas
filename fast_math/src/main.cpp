3a401c6ec89e78baed0fad648c5ff5f7a104c772
Leonardo Cazarine <leocazarine@usp.br>
2022-10-17 15:19:39 -0300

    main.cpp v0

--- fast_math/src/main.cpp
+++ fast_math/src/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <math.h>
 
-int main(int argc, char *argv[])
-{
-	std::cout << "Hello world!" << std::endl;
+using namespace std;
+
+#define S 0.1
+#define PI 3.14159265358979323846
+
+int main(float translation_x, float translation_y, float translation_z, float rotation_right_z, float rotation_up_z,float h_ref, float v_ref, float h_density, float v_density) {
+	float delta_x, delta_y;
+	float new_x, new_y;
+
+	cout << translation_x << endl;
+
+
+	delta_x = tan(rotation_right_z * PI) * translation_z;
+	delta_y = tan(rotation_up_z * PI) * translation_z;
+
+	new_x = h_ref - (translation_x + S * delta_x) * h_density;
+	new_y = v_ref - (translation_y + S * delta_y) * v_density;
+
+	cout << new_x << ", " << new_y << endl;
+
 }
