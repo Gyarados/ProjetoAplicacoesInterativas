cbbe8d2e267866ae8696de81ea73e7674b6fa560
Miguel Ferreira <miguel.ferreira@letscode.com.br>
2022-11-07 14:28:50 -0300

    all files from c# and bat file

--- exec.bat
+++ exec.bat
@@ -1,3 +1,4 @@
 start "" /d "." python ui.py
 start "" /d "." python clique2.py
-start /d "C:\Users\Inspira\AppData\Local\Programs\AR Tracking" ar-tracking.exe
+@REM start /d "C:\Users\Inspira\AppData\Local\Programs\AR Tracking" ar-tracking.exe
+start /d "C:\Users\lcode\Documents\Poli\Tecnologias Interativas\ProjetoAplicacoesInterativas\codigo_cs\bin\Debug\net6.0\" projeto.exe
