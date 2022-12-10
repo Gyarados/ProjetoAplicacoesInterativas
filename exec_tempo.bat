1b800ed4ee09e69b8b2ece061007493308669ef9
Miguel Ferreira <79666467+MFC-MiguelFerreira@users.noreply.github.com>
2022-12-10 14:09:32 -0300

    bat files

--- exec_tempo.bat
+++ exec_tempo.bat
@@ -1,4 +1,5 @@
-start "" /d "." python ui.py
-start "" /d "." python clique2.py
+@REM start "" /d "." python ui.py
+start "" /d "." python clique_tempo.py
 @REM start /d "C:\Users\Inspira\AppData\Local\Programs\AR Tracking" ar-tracking.exe
+@REM verificar possibilidade de abrir teclado virtual e navegar no uol
 start /d "C:\Users\lcode\Documents\Poli\Tecnologias Interativas\ProjetoAplicacoesInterativas\codigo_cs\bin\Debug\net6.0\" projeto.exe
